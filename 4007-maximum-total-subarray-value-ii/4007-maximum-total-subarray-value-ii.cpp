cef452d5e39ec2ce3180d5d3549169bcb8ba1ce5
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-10-22 21:44:24 +0700

    Time: 459 ms (69.69%), Space: 166 MB (78.81%) - LeetHub

--- 4007-maximum-total-subarray-value-ii/4007-maximum-total-subarray-value-ii.cpp
+++ 4007-maximum-total-subarray-value-ii/4007-maximum-total-subarray-value-ii.cpp
@@ -1,56 +1,45 @@
 #define LOG 18
 #define N 100005
-int rmq[LOG + 1][N];
+int rmqmn[LOG + 1][N];
+int rmqmx[LOG + 1][N];
 class Solution {
 public:
-    int query(int l, int r) {
+    int queryMx(int l, int r) {
         int msb = 32 - __builtin_clz(r - l + 1) - 1;
-        return max(rmq[msb][l], rmq[msb][r - (1 << msb) + 1]);
+        return max(rmqmx[msb][l], rmqmx[msb][r - (1 << msb) + 1]);
+    }
+    int queryMn(int l, int r) {
+        int msb = 32 - __builtin_clz(r - l + 1) - 1;
+        return min(rmqmn[msb][l], rmqmn[msb][r - (1 << msb) + 1]);
     }
     long long maxTotalValue(vector<int>& nums, int k) {
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) rmq[0][i] = nums[i];
+        for (int i = 0; i < n; i++) rmqmx[0][i] = rmqmn[0][i] = nums[i];
         for (int j = 1; j <= LOG; j++)
             for (int i = 0; i + (1 << j) - 1 < n; i++)
-                rmq[j][i] = max(rmq[j - 1][i], rmq[j - 1][i + (1 << (j - 1))]);
+                rmqmx[j][i] = max(rmqmx[j - 1][i], rmqmx[j - 1][i + (1 << (j - 1))]);
+        for (int j = 1; j <= LOG; j++)
+            for (int i = 0; i + (1 << j) - 1 < n; i++)
+                rmqmn[j][i] = min(rmqmn[j - 1][i], rmqmn[j - 1][i + (1 << (j - 1))]);
 
-        vector<int> L(n), R(n);
-        vector<int> st;
-        for (int i = 0; i < n; i++) {
-            while (st.size() && nums[st.back()] >= nums[i]) st.pop_back();
-            if (st.size())
-                L[i] = st.back();
-            else
-                L[i] = -1;
-            st.push_back(i);
-        }
-        st.clear();
-        auto cmp = [&](auto &v1, auto &v2) {
-            return query(v1[1], v1[2]) - nums[v1[0]] < query(v2[1], v2[2]) - nums[v2[0]];  
+        auto cmp = [&](auto &p1, auto &p2) {
+            return queryMx(p2.first, p2.second) - queryMn(p2.first, p2.second) >
+                    queryMx(p1.first, p1.second) - queryMn(p1.first, p1.second);
         };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> q(cmp);
-        set<pair<int,int>> S;
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.size() && nums[st.back()] > nums[i]) st.pop_back();
-            if (st.size())
-                R[i] = st.back();
-            else
-                R[i] = n;
-            st.push_back(i);
-            q.push({i, L[i] + 1, R[i] - 1});
-            S.insert({L[i] + 1, R[i] - 1});
-        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(cmp)> q(cmp);
+        unordered_set<long long> S;
+        q.push({0, n - 1});
+        long long base = n + 5;
+        S.insert(1 + (n - 1 + 1) * base);
         long long ans = 0;
         while (k--) {
-            auto v = q.top(); q.pop();
-            int idx = v[0];
-            int l = v[1];
-            int r = v[2];
-            ans += query(l, r) - nums[idx];
-            // cout << l << ' ' << r << ' ' << query(l, r) - nums[idx] << ' ' << idx << '\n';
-            if (l + 1 <= idx && l + 1 <= r && !S.count({l + 1, r})) q.push({idx, l + 1, r}), S.insert({l + 1, r});
-            if (r - 1 >= idx && l <= r - 1 && !S.count({l, r - 1})) q.push({idx, l, r - 1}), S.insert({l, r - 1});
+            auto p = q.top(); q.pop();
+            int l = p.first;
+            int r = p.second;
+            ans += queryMx(l, r) - queryMn(l, r);
+            if (l + 1 <= r && !S.count(l + 2 + (r + 1) * base)) q.push({l + 1, r}), S.insert(l + 2 + (r + 1) * base);
+            if (l <= r - 1 && !S.count(l + 1 + r * base)) q.push({l, r - 1}), S.insert(l + 1 + r * base);
         }
         return ans;
     }
