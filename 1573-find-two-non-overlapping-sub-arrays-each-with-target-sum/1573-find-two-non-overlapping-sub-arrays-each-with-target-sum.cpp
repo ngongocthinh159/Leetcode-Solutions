7678a23a405e6e3d680c4df380e9bf638510a867
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-10-23 21:43:08 +0700

    Time: 192 ms (27.14%), Space: 142.5 MB (30.72%) - LeetHub

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<int> pref(n + 1);
         unordered_map<int,int> f;
         f[0] = 0;
         int sum = 0;
-        vector<int> dp(n + 1, INT_MAX);
         vector<int> mn(n + 1, INT_MAX);
         int ans = INT_MAX;
         for (int i = 0; i < n; i++) {
             sum += arr[i];
 
             int val = sum - target;
+            int len = INT_MAX;
 
             if (f.count(val)) {
-                dp[i + 1] = i + 1 - f[val];
+                len = i + 1 - f[val];
                 if (mn[f[val]] != INT_MAX) {
-                    ans = min(ans, dp[i + 1] + mn[f[val]]);
+                    ans = min(ans, len + mn[f[val]]);
                 }
             }
-            mn[i + 1] = min(mn[i], dp[i + 1]);
+            mn[i + 1] = min(mn[i], len);
 
             f[sum] = i + 1;
         }
