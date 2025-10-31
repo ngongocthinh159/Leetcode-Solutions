621968842dbe1f76ea417091e1cf5da7ec2f5780
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-10-31 23:29:33 +0700

    Time: 60 ms (64.14%), Space: 47.2 MB (39.92%) - LeetHub

--- 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
+++ 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     void reduce(vector<pair<char,int>> &st, int k) {
-        while (st.size() >= 2 && st.back().first == ')' && st.back().second >= k && st[st.size() - 2].first == '(' && st[st.size() - 2].second >= k) {
+        while (st.size() >= 2 && st.back().first == ')' && st.back().second == k && st[st.size() - 2].first == '(' && st[st.size() - 2].second >= k) {
+            st.pop_back();
             st.back().second -= k;
-            st[st.size() - 2].second -= k;
-            if (st[st.size() - 2].second == 0) swap(st[st.size() - 2], st[st.size() - 1]), st.pop_back();
             if (st.back().second == 0) st.pop_back();
-            while (st.size() >= 2 && st.back().first == st[st.size() - 2].first) {
-                st[st.size() - 2].second += st.back().second;
-                st.pop_back();
-            }
         }
     }
     string removeSubstring(string s, int k) {
         int n = s.size();
         vector<pair<char,int>> st;
         string res = "";
-        for (int i = 0; i < n; ) {
+        for (int i = 0; i < n; i++) {
             char c = s[i];
-            int cnt = 0;
-            while (i < n && c == s[i]) i++, cnt++;
             if (st.size() && st.back().first == c) {
-                st.back().second += cnt;
+                st.back().second++;
             } else
-                st.push_back({c, cnt});
+                st.push_back({c, 1});
             reduce(st, k);
         }
         for (int i = 0; i < st.size(); i++)
