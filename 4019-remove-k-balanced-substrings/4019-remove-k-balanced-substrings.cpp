9f3cb194a63ec7ed8cae6a73f8c2582f0932bf6a
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-10-31 23:17:35 +0700

    Time: 41 ms (81.53%), Space: 47.3 MB (38.21%) - LeetHub

---
+++ 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void reduce(vector<pair<char,int>> &st, int k) {
+        while (st.size() >= 2 && st.back().first == ')' && st.back().second >= k && st[st.size() - 2].first == '(' && st[st.size() - 2].second >= k) {
+            st.back().second -= k;
+            st[st.size() - 2].second -= k;
+            if (st[st.size() - 2].second == 0) swap(st[st.size() - 2], st[st.size() - 1]), st.pop_back();
+            if (st.back().second == 0) st.pop_back();
+            while (st.size() >= 2 && st.back().first == st[st.size() - 2].first) {
+                st[st.size() - 2].second += st.back().second;
+                st.pop_back();
+            }
+        }
+    }
+    string removeSubstring(string s, int k) {
+        int n = s.size();
+        vector<pair<char,int>> st;
+        string res = "";
+        for (int i = 0; i < n; ) {
+            char c = s[i];
+            int cnt = 0;
+            while (i < n && c == s[i]) i++, cnt++;
+            if (st.size() && st.back().first == c) {
+                st.back().second += cnt;
+            } else
+                st.push_back({c, cnt});
+            reduce(st, k);
+        }
+        while (st.size()) {
+            for (int i = 0; i < st.back().second; i++) res += st.back().first;
+            st.pop_back();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+        
+    }
+};
