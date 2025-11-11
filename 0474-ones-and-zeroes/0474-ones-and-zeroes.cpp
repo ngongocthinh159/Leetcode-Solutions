fdd60c40755bb7d803254538fe951662a2c8056a
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-11-11 21:35:22 +0700

    0474-ONES-AND-ZEROES Stats: Time: 68 ms (67.41%), Space: 13.9 MB (66.06%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -4,16 +4,14 @@ public:
         int N = strs.size();
         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         for (int i = 0; i < N; i++) {
-            vector<vector<int>> ndp = dp;
             int Z = 0, O = 0;
             for (auto c : strs[i]) {
                 if (c == '0') Z++;
                 else O++;
             }
-            for (int z = 0; z <= m; z++)
-                for (int o = 0; o <= n; o++) if (z >= Z && o >= O)
-                    ndp[z][o] = max(ndp[z][o], 1 + dp[z - Z][o - O]);
-            swap(dp, ndp);
+            for (int z = m; z >= 0; z--)
+                for (int o = n; o >= 0; o--) if (z >= Z && o >= O)
+                    dp[z][o] = max(dp[z][o], 1 + dp[z - Z][o - O]);
         }
         int ans = 0;
         for (int z = 0; z <= m; z++)
