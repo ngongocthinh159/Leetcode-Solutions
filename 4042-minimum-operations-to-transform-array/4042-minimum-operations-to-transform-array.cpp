6cfe34d53bbd15bb5fc0c3400a5b083ad56c8fe2
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-11-14 21:00:55 +0700

    4042-MINIMUM-OPERATIONS-TO-TRANSFORM-ARRAY Stats: Time: 0 ms (100%), Space: 213.9 MB (70.78%) - LeetHub

--- 4042-minimum-operations-to-transform-array/4042-minimum-operations-to-transform-array.cpp
+++ 4042-minimum-operations-to-transform-array/4042-minimum-operations-to-transform-array.cpp
@@ -4,20 +4,17 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         long long cost = -1, last = nums2[m - 1], ans = 0;
+        long long mn = INT_MAX;
         for (int i = 0; i < n; i++) {
             int st = min(nums1[i], nums2[i]);
             int end = max(nums1[i], nums2[i]);
             if (st <= last && last <= end) cost = 0;
             ans += end - st;
+            mn = min(mn, min(abs(nums1[i] - last), abs(nums2[i] - last)));
         }
         ans++;
-        if (cost == -1) {
-            long long mn = INT_MAX;
-            for (int i = 0; i < n; i++) {
-                mn = min(mn, min(abs(nums1[i] - last), abs(nums2[i] - last)));
-            }
+        if (cost == -1)
             ans += mn;
-        }
         return ans;
     }
 };
