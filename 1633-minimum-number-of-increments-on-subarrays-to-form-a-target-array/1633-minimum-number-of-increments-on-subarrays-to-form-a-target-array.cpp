968f6caedc373d88d56b26061b5b5a3597722742
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-11-16 00:27:34 +0700

    1633-MINIMUM-NUMBER-OF-INCREMENTS-ON-SUBARRAYS-TO-FORM-A-TARGET-ARRAY Stats: Time: 0 ms (100%), Space: 76.9 MB (82.83%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -2,25 +2,10 @@ class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
         int n = target.size(), ans = 0;
-        vector<int> st;
-        vector<int> L(n), R(n);
-        for (int i = 0; i < n; i++) {
-            while (st.size() && target[st.back()] > target[i]) st.pop_back();
-            if (st.size())
-                L[i] = target[st.back()];
-            else
-                L[i] = 0;
-            st.push_back(i);
-        }
-        st.clear();
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.size() && target[st.back()] >= target[i]) st.pop_back();
-            if (st.size())
-                R[i] = target[st.back()];
-            else
-                R[i] = 0;
-            st.push_back(i);
-            ans += target[i] - max(L[i], R[i]);
+        int prev = 0;
+        for (int i = 0; i  <n ; i++) {
+            ans += max(0, target[i] - prev);
+            prev = target[i];
         }
         return ans;
     }
