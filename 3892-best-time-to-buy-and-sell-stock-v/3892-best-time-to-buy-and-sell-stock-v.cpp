41a3b0b0aadca99063e7a80ff401de018c7dfa6f
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-20 09:53:08 +0700

    3892-BEST-TIME-TO-BUY-AND-SELL-STOCK-V Stats: Time: 23 ms (77.33%), Space: 40.4 MB (66.06%) - LeetHub

--- 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,33 +1,30 @@
+#define N 1005
+long long dp[N][N/2][3]; 
 class Solution {
 public:
-    int K;
-    int n;
-    const long long LINF = 1e18 + 5;
-    vector<int> v;
-    vector<vector<vector<long long>>> dp;
-    long long dfs(int i, int k, int t) {
-        if (k > K) return -LINF;
-        if (i == n) {
-            if (t != 0) return -LINF;
-            return 0;
-        }
-        if (dp[i][k][t] != -1) return dp[i][k][t];
-        long long res = 0;
-        if (t == 0) {
-            res = max(-v[i] + dfs(i + 1, k + 1, 1), v[i] + dfs(i + 1, k + 1, 2));
-            res = max(res, dfs(i + 1, k, t));
-        } else if (t == 1) {
-            res = max(dfs(i + 1, k, t), v[i] + dfs(i + 1, k, 0));
-        } else {
-            res = max(dfs(i + 1, k, t), -v[i] + dfs(i + 1, k, 0));
-        }
-        return dp[i][k][t] = res;
-    }
     long long maximumProfit(vector<int>& prices, int k) {
-        K = k;
-        n = prices.size();
-        v = prices;
-        dp.assign(n, vector<vector<long long>>(n / 2 + 5, vector<long long>(3, -1)));
-        return dfs(0, 0, 0);
+        int n = prices.size();
+        long long ans = 0;
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= k; j++)
+                for (int t = 0; t < 3; t++) dp[i][j][t] = INT_MIN;
+        
+        dp[0][0][0] = 0;
+        dp[0][0][1] = -prices[0];
+        dp[0][0][2] = prices[0];
+        for (int i = 1; i < n; i++) {
+            long long p = prices[i];
+            dp[i][0][0] = 0;
+            dp[i][0][1] = max(dp[i - 1][0][1], -p);
+            dp[i][0][2] = max(dp[i - 1][0][2], p);
+            for (int j = 1; j <= k; j++) {
+                dp[i][j][0] = max(dp[i - 1][j][0], 
+                            max(dp[i - 1][j - 1][1] + p, dp[i - 1][j - 1][2] - p));
+                dp[i][j][1] = max(dp[i - 1][j][0] - p, dp[i - 1][j][1]);
+                dp[i][j][2] = max(dp[i - 1][j][0] + p, dp[i - 1][j][2]);
+                if (i == n - 1) ans = max(ans, dp[i][j][0]);
+            }
+        }
+        return ans;
     }
 };
