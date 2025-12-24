0f666ba9cb27a475feda869579c1a5d2106188f1
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-24 20:44:35 +0700

    1008-BINARY-TREE-CAMERAS Stats: Time: 0 ms (100%), Space: 21.1 MB (99.6%) - LeetHub

--- 1008-binary-tree-cameras/1008-binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    vector<int> dfs(TreeNode* root) {
+    array<int, 3> dfs(TreeNode* root) {
         if (!root->left && !root->right)
             return {1, 0, 0};
 
@@ -29,7 +29,6 @@ public:
         auto l = dfs(root->left);
         auto r = dfs(root->right);
         int ans0 = min(1 + l[1] + r[1], min(1 + l[2] + r[0], 1 + r[2] + l[0]));
-        // ans0 = min(ans0, 1 + l[2] + 1 + r[2]);
         int ans1 = min(l[0] + r[0], 1 + l[1] + r[1]);
         int ans2 = l[1] + r[1];
         return {ans0, ans1, ans2};
