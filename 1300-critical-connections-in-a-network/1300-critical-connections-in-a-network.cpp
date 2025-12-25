691dd0b1c3ea1b70237e742ef9be6c234bd9e438
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-25 13:29:58 +0700

    1300-CRITICAL-CONNECTIONS-IN-A-NETWORK Stats: Time: 211 ms (39.4%), Space: 182.2 MB (56.64%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    vector<int> dp;
+    vector<int> lowlink;
     vector<int> color;
-    vector<int> up;
-    vector<int> down;
+    vector<int> depth;
     vector<vector<int>> g;
     vector<vector<int>> ans;
-    void dfs(int u, int p, vector<vector<int>> &e) {
+    void dfs(int u, int p) {
         color[u] = 1;
-        int sumc = 0;
-        for (auto idx : g[u]) {
-            int v = e[idx][0] ^ e[idx][1] ^ u;
-            if (v != p) {
-                if (color[v] == 1) {
-                    up[u]++;
-                    down[v]++;
-                } else if (color[v] == 0) {
-                    dfs(v, u, e);
-                    sumc += dp[v];
-                    if (dp[v] == 0) ans.push_back({u, v});
-                }
+        for (auto v : g[u]) if (v != p) {
+            if (color[v] == 0) {
+                depth[v] = depth[u] + 1;
+                dfs(v, u);
+                lowlink[u] = min(lowlink[u], lowlink[v]);
+            } else if (color[v] == 1) {
+                lowlink[u] = min(lowlink[u], depth[v]);
             }
         }
-        dp[u] = up[u] - down[u] + sumc;
+        if (p != -1 && lowlink[u] >= depth[u]) ans.push_back({u, p});
         color[u] = 2;
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        dp.resize(n);
+        lowlink.assign(n, INT_MAX);
+        g.resize(n);
+        depth.resize(n);
         color.resize(n);
-        up.resize(n);
-        down.resize(n);
-        g.assign(n, vector<int>());
-        for (int i = 0; i < connections.size(); i++) {
-            auto &c = connections[i];
-            g[c[0]].push_back(i);
-            g[c[1]].push_back(i);
+        for (auto &c : connections) {
+            g[c[0]].push_back(c[1]);
+            g[c[1]].push_back(c[0]);
         }
-        dfs(0, -1, connections);
+        dfs(0, -1);
         return ans;
     }
 };
