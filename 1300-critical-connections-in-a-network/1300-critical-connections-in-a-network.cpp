fc9449e341d94b79af73afdc6c8444aad793a48b
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-25 13:33:19 +0700

    1300-CRITICAL-CONNECTIONS-IN-A-NETWORK Stats: Time: 177 ms (67.3%), Space: 181.1 MB (59.62%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    vector<int> lowlink;
     vector<int> color;
     vector<int> depth;
     vector<vector<int>> g;
     vector<vector<int>> ans;
-    void dfs(int u, int p) {
+    int dfs(int u, int p) {
         color[u] = 1;
+        int lowu = INT_MAX;
         for (auto v : g[u]) if (v != p) {
             if (color[v] == 0) {
                 depth[v] = depth[u] + 1;
-                dfs(v, u);
-                lowlink[u] = min(lowlink[u], lowlink[v]);
+                lowu = min(lowu, dfs(v, u));
             } else if (color[v] == 1) {
-                lowlink[u] = min(lowlink[u], depth[v]);
+                lowu = min(lowu, depth[v]);
             }
         }
-        if (p != -1 && lowlink[u] >= depth[u]) ans.push_back({u, p});
+        if (p != -1 && lowu >= depth[u]) ans.push_back({u, p});
         color[u] = 2;
+        return lowu;
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        lowlink.assign(n, INT_MAX);
         g.resize(n);
         depth.resize(n);
         color.resize(n);
