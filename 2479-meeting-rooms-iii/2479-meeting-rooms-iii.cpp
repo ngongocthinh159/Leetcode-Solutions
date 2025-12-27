b4ca5bbad48b63fad7b12a3c3dfe06041f2c8da3
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-27 10:16:09 +0700

    2479-MEETING-ROOMS-III Stats: Time: 227 ms (6.89%), Space: 149.1 MB (5.09%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -21,36 +21,35 @@ public:
         long long time, type, mid, rid;
         vector<int> cnt(n);
         while(q.size()) {
-            auto e = q.top();
-            time = e.time, type = e.type, mid = e.mid, rid = e.rid;
-            q.pop();
-            if (type == 1) {
-                if (avail.size()) {
-                    int used = *avail.begin();
-                    avail.erase(avail.begin());
-                    cnt[used]++;
-                    q.push(event{meetings[mid][1], 2, mid, used});
-                    // cout << "meeting " << mid << " used room " << used << "\n"; 
-                } else {
+            long long ctime = q.top().time;
+
+            while (q.size() && q.top().time == ctime && q.top().type == 2) {
+                avail.insert(q.top().rid);
+                q.pop();
+            }
+
+            while (delay.size() && avail.size()) {
+                int ridused = *avail.begin();
+                avail.erase(avail.begin());
+                int midused = delay.back();
+                delay.pop_back();
+                cnt[ridused]++;
+                q.push(event{ctime - meetings[midused][0] + meetings[midused][1], 2, midused, ridused});
+            }
+
+            while (q.size() && q.top().time == ctime && q.top().type == 1) {
+                auto &e = q.top();
+                time = e.time, type = e.type, mid = e.mid, rid = e.rid;
+                if (avail.empty()) {
                     delay.push_front(mid);
-                    // cout << "meeting " << mid << " delayed " << "\n"; 
-                }
-                // cout << "\n";
-            } else {
-                // cout << "room " << rid << " become avail" << "\n";
-                avail.insert(rid);
-                while (q.size() && q.top().time == time && q.top().type == 2) {
-                    avail.insert(q.top().rid);
-                    q.pop();
-                }
-                while (delay.size() && avail.size()) {
-                    int used = *avail.begin();
+                } else {
+                    int ridused = *avail.begin();
                     avail.erase(avail.begin());
-                    int dmid = delay.back();
-                    delay.pop_back();
-                    cnt[used]++;
-                    q.push({event{meetings[dmid][1] - meetings[dmid][0] + time, 2, dmid, used}});
+                    cnt[ridused]++;
+                    q.push(event{meetings[mid][1], 2, mid, ridused});
                 }
+
+                q.pop();
             }
         }
         int mx = -1, idx;
