2422aa894ee7491f2f26f84c73f0ef1bd47d1c0e
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-27 10:18:04 +0700

    2479-MEETING-ROOMS-III Stats: Time: 227 ms (6.89%), Space: 149.2 MB (5.09%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -21,13 +21,16 @@ public:
         long long time, type, mid, rid;
         vector<int> cnt(n);
         while(q.size()) {
+            // process of events of ctime
             long long ctime = q.top().time;
 
+            // process all events of type 2 first
             while (q.size() && q.top().time == ctime && q.top().type == 2) {
                 avail.insert(q.top().rid);
                 q.pop();
             }
 
+            // assign rooms for all delayed meetings
             while (delay.size() && avail.size()) {
                 int ridused = *avail.begin();
                 avail.erase(avail.begin());
@@ -37,6 +40,7 @@ public:
                 q.push(event{ctime - meetings[midused][0] + meetings[midused][1], 2, midused, ridused});
             }
 
+            // assign rooms for all original meeting happened at ctime
             while (q.size() && q.top().time == ctime && q.top().type == 1) {
                 auto &e = q.top();
                 time = e.time, type = e.type, mid = e.mid, rid = e.rid;
@@ -53,9 +57,7 @@ public:
             }
         }
         int mx = -1, idx;
-        for (int i = n - 1; i >= 0; i--) {
-            if (mx <= cnt[i]) mx = cnt[i], idx = i;
-        }
+        for (int i = n - 1; i >= 0; i--) if (mx <= cnt[i]) mx = cnt[i], idx = i;
         return idx;
     }
 };
