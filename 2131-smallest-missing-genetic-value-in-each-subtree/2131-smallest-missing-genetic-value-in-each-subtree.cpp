628b5bdbcb85c0d00a9e11078b23af2ffbfc72fb
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2025-12-27 08:59:43 +0700

    2131-SMALLEST-MISSING-GENETIC-VALUE-IN-EACH-SUBTREE Stats: Time: 659 ms (26.78%), Space: 232.9 MB (31.25%) - LeetHub

--- 2131-smallest-missing-genetic-value-in-each-subtree/2131-smallest-missing-genetic-value-in-each-subtree.cpp
+++ 2131-smallest-missing-genetic-value-in-each-subtree/2131-smallest-missing-genetic-value-in-each-subtree.cpp
@@ -10,10 +10,10 @@ public:
         ans.resize(n);
         mn.resize(n);
         for (int i = 1; i < n; i++) g[parents[i]].push_back(i);
-        int mx = n;
-        vector<bool> ok(n);
-        for (auto x : nums) if (x - 1 < n) ok[x - 1] = 1;
-        for (int i = 0; i < n; i++) if (!ok[i]) {
+        int mx = n + 1;
+        vector<bool> ok(n + 1);
+        for (auto x : nums) if (x <= n) ok[x] = 1;
+        for (int i = 1; i <= n; i++) if (!ok[i]) {
             mx = i;
             break;
         }
@@ -22,14 +22,14 @@ public:
         return ans;
     }
     void dfs0(int u, vector<int> &nums) {
-        mn[u] = nums[u] - 1;
+        mn[u] = nums[u];
         for (auto v : g[u]) {
             dfs0(v, nums);
             mn[u] = min(mn[v], mn[u]);
         }
     }
     void dfs(int u, int x, vector<int> &nums) {
-        ans[u] = x + 1;
+        ans[u] = x;
         int sz = g[u].size();
         vector<int> pref(sz + 1); 
         vector<int> surf(sz + 2); 
@@ -39,7 +39,7 @@ public:
         for (int i = sz - 1; i >= 0; i--)
             surf[i + 1] = min(surf[i + 2], mn[g[u][i]]);
         for (int i = 0; i < sz; i++) {
-            int nxt_x = min(x, min(nums[u] - 1, min(pref[i], surf[i + 2])));
+            int nxt_x = min(x, min(nums[u], min(pref[i], surf[i + 2])));
             dfs(g[u][i], nxt_x, nums);
         }
     }
