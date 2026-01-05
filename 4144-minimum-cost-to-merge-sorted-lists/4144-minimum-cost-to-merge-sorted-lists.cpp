9511dcb1bac80194a830ef5624baa6383fe0d742
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-01-05 21:31:42 +0700

    4144-MINIMUM-COST-TO-MERGE-SORTED-LISTS Stats: Time: 1891 ms (52.63%), Space: 47.1 MB (84.21%) - LeetHub

--- 4144-minimum-cost-to-merge-sorted-lists/4144-minimum-cost-to-merge-sorted-lists.cpp
+++ 4144-minimum-cost-to-merge-sorted-lists/4144-minimum-cost-to-merge-sorted-lists.cpp
@@ -1,31 +1,20 @@
 #define ll long long
 #define N 12
+#define M 2005
 ll dp[1 << N], median[1 << N], len[1 << N];
+int buf[M];
 class Solution {
 public:
-    void merge(vector<int>&a, vector<int>&b) {
-        int n = a.size(), m = b.size();
-        vector<int> c(n + m);
-        int i = 0, j = 0, k = 0;
-        while (i < n && j < m) {
-            if (a[i] <= b[j]) c[k++] = a[i++];
-            else c[k++] = b[j++];
-        }
-        while (i < n) c[k++] = a[i++];
-        while (j < m) c[k++] = b[j++];
-        swap(a, c);
-    }
+    int top;
     long long minMergeCost(vector<vector<int>>& lists) {
         int n = lists.size();
         for (int mask = 1; mask < (1 << n); mask++) {
-            int clen = 0, cmedian = 0;
-            vector<int> cur;
-            for (int bit = 0; bit < n; bit++) if ((mask >> bit) & 1) {
-                merge(cur, lists[bit]), clen += (int) lists[bit].size();
-            }
-            cmedian = cur[(clen + 1)/2 - 1];
-            len[mask] = clen;
-            median[mask] = cmedian;
+            top = 0;
+            for (int bit = 0; bit < n; bit++) if ((mask >> bit) & 1)
+                for (auto x : lists[bit]) buf[top++] = x;
+            sort(buf, buf + top);
+            len[mask] = top;
+            median[mask] = buf[(top + 1)/2 - 1];
         }
         for (int mask = 1; mask < (1 << n); mask++) if (__builtin_popcount(mask) > 1) {
             dp[mask] = LLONG_MAX;
