b0aaf207e0dacfc745d2e7e13a9ce65a0ca8ca40
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-01-08 23:52:05 +0700

    1569-MAX-DOT-PRODUCT-OF-TWO-SUBSEQUENCES Stats: Time: 14 ms (78%), Space: 20.9 MB (10%) - LeetHub

--- 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
@@ -4,17 +4,13 @@ public:
     const int IINF = 1e9;
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<vector<vector<ll>>> dp(n + 1, 
-            vector<vector<ll>>(m + 1, 
-                vector<ll>(2, -IINF)));
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= m; j++) dp[i][j][0] = 0;
+        vector<vector<ll>> dp(n + 1, vector<ll>(m + 1, -IINF));
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= m; j++) {
-                dp[i][j][1] = max(dp[i][j][1], nums1[i - 1] * nums2[j - 1] + max(dp[i - 1][j - 1][0], dp[i - 1][j - 1][1]));
-                dp[i][j][1] = max(dp[i][j][1], dp[i - 1][j][1]);
-                dp[i][j][1] = max(dp[i][j][1], dp[i][j - 1][1]);
+                dp[i][j] = max(dp[i][j], nums1[i - 1] * nums2[j - 1] + max(0LL, dp[i - 1][j - 1]));
+                dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = max(dp[i][j], dp[i][j - 1]);
             }
-        return dp[n][m][1];
+        return dp[n][m];
     }
 };
