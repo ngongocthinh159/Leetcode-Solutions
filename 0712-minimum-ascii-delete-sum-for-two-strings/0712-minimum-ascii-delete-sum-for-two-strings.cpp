4da329aeae73cbae127d9559310ded75d5797e39
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-01-10 15:57:15 +0700

    0712-MINIMUM-ASCII-DELETE-SUM-FOR-TWO-STRINGS Stats: Time: 19 ms (44.45%), Space: 18 MB (76.32%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -8,11 +8,15 @@ public:
         for (int j = 1; j <= m; j++) dp[0][j] = s2[j - 1] + dp[0][j - 1];
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= m; j++) {
-                if (s1[i - 1] == s2[j - 1])
+                if (s1[i - 1] == s2[j - 1]) {
+
                     dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + s1[i - 1] + s2[j - 1]);
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + s1[i - 1]);
-                dp[i][j] = min(dp[i][j], dp[i][j - 1] + s2[j - 1]);
+                } else {
+
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + s1[i - 1] + s2[j - 1]);
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + s1[i - 1]);
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + s2[j - 1]);
+                }
             }
         return dp[n][m];
     }
