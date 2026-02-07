4a953044725dc88f7af79232f5661ef98818d2da
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-07 15:31:44 +0700

    Time: 21 ms (67.46%), Space: 25.5 MB (87.57%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -3,14 +3,16 @@ public:
     const int IINF = 1e9 + 5;
     int minimumDeletions(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2));
-        dp[0][0] = 0, dp[0][1] = IINF;
+        int p0 = 0, p1 = IINF;  
         for (int i = 1; i <= n; i++) {
             char c = s[i - 1] - 'a';
-            dp[i][0] = dp[i - 1][0] + (c != 0);
-            if (c == 0) dp[i][1] = dp[i - 1][1] + 1;
-            else dp[i][1] = min(dp[i - 1][0], dp[i - 1][1]);
+            int np0, np1;
+            np0 = p0 + (c != 0);
+            if (c == 0) np1 = p1 + 1;
+            else np1 = min(p0, p1);
+            swap(p0, np0);
+            swap(p1, np1);
         }
-        return min(dp[n][0], dp[n][1]);
+        return min(p0, p1);
     }
 };
