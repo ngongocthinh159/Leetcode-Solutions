6a34f036c6ad64a53bcd190a2f47fd28d3f3533d
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-12 18:49:29 +0700

    Time: 47 ms (96.24%), Space: 11.5 MB (92.68%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -4,16 +4,14 @@ public:
         int n = s.size(), ans = 0;
         int cnt[26]{};
         for (int i = 0; i < n; i++) {
-            unordered_map<int,int> f;
+            int u = 0, mx = -1;
             for (int j = i; j >= 0; j--) {
                 int idx = s[j] - 'a';
-                if (cnt[idx] && --f[cnt[idx]] == 0)
-                    f.erase(cnt[idx]);
-                cnt[idx]++;
-                f[cnt[idx]]++;
-                if (f.size() == 1) ans = max(ans, i - j + 1);
+                if (++cnt[idx] == 1) u++;
+                mx = max(mx, cnt[idx]);
+                if ((i - j + 1) % u == 0 && (i - j + 1)/u == mx) ans = max(ans, i - j + 1);
             }
-            for (int j = i; j >= 0; j--) cnt[s[j] - 'a'] = 0;
+            for (int i = 0; i < 26; i++) cnt[i] = 0;
         }
         return ans;
     }
