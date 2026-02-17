3a4ff0a40b17d59f2586d51dcab1fc5c8a27d01c
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-17 12:27:55 +0700

    Time: 3 ms (71.7%), Space: 18.1 MB (33.16%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<vector<double>> dp;
-    double dfs(int poured, int i, int j) {
-        if (i == 1) return poured;
-        if (dp[i][j] != -1.0) return dp[i][j];
-
-        double res;
-        if (j == 1) {   
-            res = max(0.0, dfs(poured, i - 1, j) - 1) / 2.00;    
-        } else if (j == i) {
-            res = max(0.0, dfs(poured, i - 1, j - 1) - 1) / 2.00;
-        } else {
-            res = max(0.0, dfs(poured, i - 1, j - 1) - 1) / 2.00 + max(0.0, dfs(poured, i - 1, j) - 1) / 2.00;
-        }
-        return dp[i][j] = res;
-    }
     double champagneTower(int poured, int query_row, int query_glass) {
-        dp.assign(query_row + 2, vector<double>(query_glass + 2, -1));
-        return min(dfs(poured, query_row + 1, query_glass + 1), 1.0);
+        dp.assign(query_row + 2, vector<double>(query_row + 2, 0));
+        dp[0][0] = poured;
+        for (int i = 0; i < query_row; i++)
+            for (int j = 0; j <= i; j++) if (dp[i][j] > 1.00) {
+                dp[i][j] -= 1.00;
+                dp[i + 1][j] += dp[i][j] / 2.00;
+                dp[i + 1][j + 1] += dp[i][j] / 2.00;
+            }
+        return min(dp[query_row][query_glass], 1.00);
     }
 };
