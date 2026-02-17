1013415dfda4bfcaff5a64de1897dbca98e39487
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-17 12:42:03 +0700

    Time: 0 ms (100%), Space: 8.4 MB (99%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,15 +1,16 @@
+#define N 1005
+double dp[N];
 class Solution {
 public:
-    vector<vector<double>> dp;
     double champagneTower(int poured, int query_row, int query_glass) {
-        dp.assign(query_row + 2, vector<double>(query_row + 2, 0));
-        dp[0][0] = poured;
-        for (int i = 0; i < query_row; i++)
-            for (int j = 0; j <= i; j++) if (dp[i][j] > 1.00) {
-                dp[i][j] -= 1.00;
-                dp[i + 1][j] += dp[i][j] / 2.00;
-                dp[i + 1][j + 1] += dp[i][j] / 2.00;
+        dp[0] = poured;
+        for (int i = 1; i <= query_row; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (j == 0) dp[j] = max(0.0, dp[j] - 1) / 2.0;
+                else if (j == i) dp[j] = max(0.0, dp[j - 1] - 1) / 2.0;
+                else dp[j] = max(0.0, dp[j - 1] - 1) / 2.0 + max(0.0, dp[j] - 1) / 2.0;
             }
-        return min(dp[query_row][query_glass], 1.00);
+        }
+        return min(dp[query_glass], 1.00);
     }
 };
