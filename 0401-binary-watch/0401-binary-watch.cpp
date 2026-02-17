f053e9f016f84ecf96c1f9a565a46536f9843f8b
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-17 12:55:54 +0700

    Time: 0 ms (100%), Space: 10 MB (24.16%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.cpp
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -1,20 +1,25 @@
+#define N 11
+vector<string> res[N];
+auto init = []{
+    int fmask = (1 << 10) - 1;
+    for (int mask = 0; mask <= fmask; mask++) {
+        int turnedOn = __builtin_popcount(mask);
+        if (turnedOn < N) {
+            int h = mask >> 6;
+            int m = mask & ((1 << 6) - 1);
+            if (h <= 11 && m <= 59) {
+                string hs = to_string(h);
+                string ms = to_string(m);
+                if (ms.size() != 2) ms = "0" + ms;
+                res[turnedOn].push_back(hs + ":" + ms);
+            }
+        }
+    }
+    return 0;
+}();
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
-        vector<string> res;
-        int fmask = (1 << 10) - 1;
-        for (int mask = 0; mask <= fmask; mask++) {
-            if (__builtin_popcount(mask) == turnedOn) {
-                int h = mask >> 6;
-                int m = mask & ((1 << 6) - 1);
-                if (h <= 11 && m <= 59) {
-                    string hs = to_string(h);
-                    string ms = to_string(m);
-                    if (ms.size() != 2) ms = "0" + ms;
-                    res.push_back(hs + ":" + ms);
-                }
-            }
-        }
-        return res;
+        return res[turnedOn];
     }
 };
