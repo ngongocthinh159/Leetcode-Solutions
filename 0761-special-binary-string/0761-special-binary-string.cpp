51b60b6e49ca1db58ba72cdfa1a91383146fa9de
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-02-20 14:49:01 +0700

    Time: 0 ms (100%), Space: 9.3 MB (80.52%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.cpp
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    string makeLargestSpecial(string s) {
-        int n = s.size();
-        auto find = [&](int i, string &t) {
-            int open = -1, j;
-            while (i < n && open != 0) {
-                j = i;
-                if (open == -1) open = 0;
-                if (t[i] == '1') open++;
+    string dfs(const string &s) {
+        if (!s.size()) return "";
+        
+        int i = 0, n = s.size();
+        vector<string> v;
+        while (i < n) {
+            int open = INT_MAX, st = i, end = i;
+            while (i < n && (open != 0 || open == INT_MAX)) {
+                end = i;
+                if (open == INT_MAX) open = 0;
+                if (s[i] == '1') open++;
                 else open--;
-                if (open < 0) return -1;
                 i++;
             }
-            if (open == 0) return j;
-            return -1;
-        };
-        auto swap_str = [](int l, int m, int r, string &s) {
-            // shift left
-            reverse(s.begin() + l, s.begin() + m + 1);
-            reverse(s.begin() + m + 1, s.begin() + r + 1);
-            reverse(s.begin() + l, s.begin() + r + 1);
-        };
-        while (1) {
-            bool found = false;
-
-            for (int i = 0; i < n - 1; i ++) {
-                int j = find(i, s);
-                // cout << i << ' ' << j << '\n';
-                if (j == -1 || j == n - 1) continue;
-                int k = find(j + 1, s);
-                // cout << i << ' ' << j << ' ' << k << '\n';
-                if (k != -1) {
-                    if (s.substr(i, j - i + 1) < s.substr(j + 1, k - (j + 1) + 1)) {
-                        swap_str(i, j, k, s);
-                        // cout << s << '\n';
-                        found = true;
-                    }
-                }
-                if (found) break;
-            }
-
-            if (!found) break;
+            v.push_back("1" + dfs(s.substr(st + 1, end - st - 1)) + "0");
         }
-        return s;
+        sort(v.begin(), v.end(), greater<>());
+        string res = "";
+        for (auto &str : v) res +=  str;
+        return res;
+    }
+    string makeLargestSpecial(string s) {
+        return dfs(s);
     }
 };
