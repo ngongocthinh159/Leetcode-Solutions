484a3cc119b59658930d63548c1b87ff428cb982
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-03-08 15:24:49 +0700

    Time: 4 ms (97.47%), Space: 27.5 MB (95.58%) - LeetHub

--- 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
@@ -10,20 +10,18 @@ public:
         }
         return true;
     }
-    string sortRange(string s, const vector<vector<int>> &v) {
-        for (auto &t : v) {
-            sort(s.begin() + t[0], s.begin() + t[1] + 1);
-        }
-        return s;
-    }
     int minOperations(string s) {
         int n = s.size();
+        if (isSorted(s)) return 0;
         if (n == 2)
             return isSorted(s) ? 0 : -1;
-        if (isSorted(s)) return 0;
-        if (isSorted(sortRange(s, {{0, n - 2}})) || isSorted(sortRange(s, {{1, n - 1}}))) return 1;
-        if (isSorted(sortRange(s, {{0, n - 2}, {1, n - 1}})) || 
-            isSorted(sortRange(s, {{1, n - 1}, {0, n - 2}}))) return 2;
+        int mn = 333, mx = -1;
+        for (auto c : s) {
+            mn = min(mn, int(c));
+            mx = max(mx, int(c));
+        }
+        if (s[0] == mn || s[n - 1] == mx) return 1;
+        for (int i = 1; i < n - 1; i ++) if (s[i] == mn || s[i] == mx) return 2;
         return 3;
     }
 };
