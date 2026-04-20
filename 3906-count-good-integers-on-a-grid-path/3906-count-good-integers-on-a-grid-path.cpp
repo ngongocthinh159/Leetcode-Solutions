f57fdbf8a0522b4ea162491edb61ecb081108e1c
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-04-20 20:08:17 +0700

    Time: 35 ms (19.62%), Space: 22 MB (16.03%) - LeetHub

--- 3906-count-good-integers-on-a-grid-path/3906-count-good-integers-on-a-grid-path.cpp
+++ 3906-count-good-integers-on-a-grid-path/3906-count-good-integers-on-a-grid-path.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    set<int> S;
+    bool S[16];
     string d;
     vector<vector<vector<long long>>> dp;
     long long dfs(int i, int smaller, int last, vector<int> &b) {
         if (i >= b.size()) return 1;
         if (dp[i][smaller][last] != -1) return dp[i][smaller][last];
         int upper = smaller ? 9 : b[i];
-        int lower = S.count(i) ? last : 0;
+        int lower = S[i] ? last : 0;
         long long res = 0;
         for (int j = lower; j <= upper; j++) {
             int nsmaller = smaller | (j < b[i]);
-            int nlast = S.count(i) ? j : last;
+            int nlast = S[i] ? j : last;
             res += dfs(i + 1, nsmaller, nlast, b);
         }
         return dp[i][smaller][last] = res;
@@ -34,12 +34,13 @@ public:
     }
     long long countGoodIntegersOnPath(long long l, long long r, string directions) {
         d = directions;
+        for (int i = 0; i < 16; i++) S[i] = 0;
+        S[0] = 1;
         int i = 0, j = 0;
-        S.insert(0);
         for (auto c : directions) {
             if (c == 'D') i++;
             else j++;
-            S.insert(4*i + j);
+            S[4*i + j] = 1;
         }
         dp.assign(20, vector<vector<long long>>(2, vector<long long>(10)));
         return solve(r) - solve(l - 1);    
