ec7f0ce9a60d5eb295e6581d0bd48928f6dcf9c4
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-04-27 09:16:28 +0700

    Time: 299 ms (6.45%), Space: 143.5 MB (5.24%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    set<pair<int,int>> active;
-    vector<vector<array<int,2>>> moves = {
-        {},
-        {{0,-1}, {0,1}},
-        {{-1,0}, {1,0}},
-        {{0,-1}, {1,0}},
-        {{0,1}, {1,0}},
-        {{-1,0}, {0,-1}},
-        {{-1,0}, {0,1}},
-    };
-    bool ok;
-    void dfs(int i, int j, vector<vector<int>> &grid) {
-        if (i == int(grid.size() - 1) && j == int(grid[0].size() - 1)) {
-            ok = true;
-            return;
-        }
-        active.insert({i,j});
-        for (auto &move : moves[grid[i][j]]) {
-            int I = i + move[0];
-            int J = j + move[1];
-            if (0 <= I && I < int(grid.size()) && 0 <= J && J < int(grid[0].size()) && !active.count({I, J})) {
-                bool can = false;
-                for (auto &move1 : moves[grid[I][J]]) {
-                    int II = I + move1[0];
-                    int JJ = J + move1[1];
-                    if (II == i && JJ == j) {
-                        can = true;
-                        break;
+    bool hasValidPath(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<array<int,2>>> moves = {
+            {},
+            {{0,-1}, {0,1}},
+            {{-1,0}, {1,0}},
+            {{0,-1}, {1,0}},
+            {{0,1}, {1,0}},
+            {{-1,0}, {0,-1}},
+            {{-1,0}, {0,1}},
+        };
+        vector<vector<int>> g(n*m);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                for (auto &move : moves[grid[i][j]]) {
+                    int I = i + move[0];
+                    int J = j + move[1];
+                    if (0 <= I && I < n && 0 <= J && J < m) {
+                        for (auto &move1 : moves[grid[I][J]]) {
+                            int II = I + move1[0];
+                            int JJ = J + move1[1];
+                            if (II == i && JJ == j) {
+                                g[i*m + j].push_back(I*m + J);
+                            }
+                        }
                     }
                 }
-                if (can)
-                    dfs(I, J, grid);
-                if (ok) break;
+            }
+        vector<bool> vis(n*m);
+        vis[0] = 1;
+        deque<int> q;
+        q.push_front(0);
+        while (q.size()) {
+            for (int sz = q.size(); sz; sz--) {
+                int u = q.back(); q.pop_back();
+                for (auto v : g[u]) if (!vis[v]) {
+                    vis[v] = 1;
+                    q.push_front(v);
+                }
             }
         }
-        active.erase({i,j});
-    }
-    bool hasValidPath(vector<vector<int>>& grid) {
-        ok = false;
-        dfs(0,0,grid);
-        return ok;
+        return vis[(n - 1) * m + m - 1];
     }
 };
