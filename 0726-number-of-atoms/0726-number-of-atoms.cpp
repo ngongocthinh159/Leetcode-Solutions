c322c73102ce51dd56d20220770adea170976184
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-16 20:02:43 +0700

    Time: 0 ms (100%), Space: 9.5 MB (92.13%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.cpp
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
-    unordered_map<string,int> dfs(int L, int R, string &s, vector<int> &right) {
-        unordered_map<string,int> f;
-        for (int i = L; i <= R;) {
-            if (s[i] == '(') {
-                auto f2 = dfs(i + 1, right[i] - 1, s, right);
-                i = right[i] + 1;
-                int mul = 0;
-                while (i <= R && '0' <= s[i] && s[i] <= '9') {
-                    mul = mul * 10 + s[i] - '0';
-                    i++;
-                }
-                mul = max(mul, 1);
-                for (auto [c, cnt] : f2) {
-                    f[c] += cnt * mul;
-                }
-            } else {
-                string t = "";
-                t += s[i];
-                i++;
-                while (i <= R && 'a' <= s[i] && s[i] <= 'z') {
-                    t += s[i];
-                    i++;
-                }
-                int cur = 0;
-                while (i <= R && '0' <= s[i] && s[i] <= '9') {
-                    cur = cur * 10 + s[i] - '0';
-                    i++;
-                }
-                cur = max(cur, 1);
-                f[t] += cur;
-            }
-        }
-        return f;
+    int getNum(int &i, string &s) {
+        int x = 0, n = s.size();
+        while (i < n && '0' <= s[i] && s[i] <= '9') x = x * 10 + s[i++] - '0';
+        return max(x, 1);
+    }
+    string getStr(int &i, string &s) {
+        int n = s.size();
+        string t = "";
+        t += s[i++];
+        while (i < n && 'a' <= s[i] && s[i] <= 'z') t += s[i++];
+        return t;
     }
     string countOfAtoms(string s) {
-        int n = s.size();
-        vector<int> right(n);
-        vector<int> st;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') st.push_back(i);
-            else if (s[i] == ')') {
-                right[st.back()] = i;
-                st.pop_back();
+        int n = s.size(), i = 0;
+        vector<map<string,int>> st;
+        st.push_back(map<string,int>());
+        while (i < n) {
+            if (s[i] == '(') {
+                st.push_back(map<string,int>());
+                i++;
+            } else if (s[i] == ')') {
+                i++;
+                int mul = getNum(i, s);
+
+                // merge
+                if (st.size() > 1) {
+                    auto &f2 = st[st.size() - 1];
+                    auto &f = st[st.size() - 2];
+                    for (auto &[t, cnt] : f2) f[t] += cnt * mul;
+                    st.pop_back();
+                }
+            } else {
+                string t = getStr(i, s);
+                int mul = getNum(i, s);
+                auto &f = st.back();
+                f[t] += mul;
             }
         }
-        auto f = dfs(0, n - 1, s, right);
-        vector<pair<string,int>> v;
-        for (auto &[t, cnt] : f) v.push_back({t, cnt});
-        sort(v.begin(), v.end(), [&](auto &a, auto &b) {return a.first < b.first;});
         string res = "";
-        for (auto &[t, cnt] : v) {
+        for (auto &[t, cnt] : st[0]) {
             res += t;
             if (cnt > 1) res += to_string(cnt);
         }
