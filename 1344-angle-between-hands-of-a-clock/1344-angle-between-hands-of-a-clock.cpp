c2b7d275321954646fa454df95352c9a509ecb83
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-18 20:22:07 +0700

    Time: 0 ms (100%), Space: 8.2 MB (10.08%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double deg1;
-        if (0 <= minutes && minutes <= 15) {
-            deg1 = (15 - minutes) * 6;
-        }
-        else {
-            deg1 = (45 - (minutes - 15)) * 6 + 90;
-        }
+        double deg1 = minutes * 6;
         if (hour == 12) hour = 0;
-        double deg2;
-        if (0 <= hour && hour <= 3) {
-            deg2 = (3 - hour) * 30;
-        } else {
-            deg2 = (9 - (hour - 3)) * 30 + 90;
-        }
-        deg2 -= 1.00 * minutes/60 * 30;
-        if (deg2 < 0) deg2 += 360;
-        double ans;
-        if (deg1 >= deg2) ans = deg1 - deg2;
-        else ans = deg2 - deg1;
-        if (ans > 180.0) ans = 360.0 - ans;
-        // cout << deg1 << ' ' << deg2 << '\n';
-        return ans;
+        double deg2 = hour * 30 + minutes * 1.00 / 60 * 30;
+        double dif = abs(deg1 - deg2);
+        return min(dif, 360 - dif);
     }
 };
