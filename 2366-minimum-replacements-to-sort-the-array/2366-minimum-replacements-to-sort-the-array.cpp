209b2cf63f1b3e847c915608780bb63df20c3994
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-18 21:25:21 +0700

    Time: 0 ms (100%), Space: 58.5 MB (33.79%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,3 +1,7 @@
+// Go from right to left, we only need to relax a number if it is larger than the next number
+// Optimal strategy to relax a number X with cap is Y (next number) so that smallest number is maximize:
+// - We always have to divide to k = ceil(X / Y) partition -> this is fixed
+// - After have k fixed, we can find smallest = floor(X / k)
 #define ll long long
 class Solution {
 public:
@@ -12,7 +16,8 @@ public:
                 continue;
             }
             oper += nums[i] / nums[i + 1];
-            nums[i] = nums[i] / ((nums[i] + nums[i + 1] - 1) / nums[i + 1]);
+            ll k = (nums[i] + nums[i + 1] - 1) / nums[i + 1];
+            nums[i] = nums[i] / k;
         }
         return oper;
     }
