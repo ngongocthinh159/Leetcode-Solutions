def61f1f828dd71713b0d23558a14f6d80a80a55
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-21 22:16:38 +0700

    Time: 220 ms (5.08%), Space: 232.9 MB (5.31%) - LeetHub

---
+++ 3971-maximum-total-value/3971-maximum-total-value.cpp
@@ -0,0 +1,65 @@
+#define ll long long
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    int maxTotalValue(vector<int>& value, vector<int>& decay, int oper) {
+        ll n = value.size();
+        ll ans = 0;
+        ll l = -1, r = *max_element(value.begin(), value.end()) + 1;
+        ll totOper = 0, totGain = 0;
+        while (r - l > 1) {
+            ll m = l + (r - l)/2;
+
+            bool ok = true;
+            ll curOper = 0;
+            ll curGain = 0;
+            for (ll i = 0; i < n; i++) {
+                if (value[i] <= m) continue;
+                ll diff = value[i] - m;
+                ll time = (diff + decay[i] - 1) / decay[i];
+
+                curOper += time;
+                if (time > 0) {
+                    ll start = value[i] - (time - 1) * decay[i];
+                    ll end = value[i];
+                    curGain += (end + start) % MOD * time % MOD * 500000004ll % MOD;
+                    curGain %= MOD;
+                }
+                if (curOper > oper) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok) {
+                totGain = curGain;
+                totOper = curOper;
+                r = m;
+            }
+            else
+                l = m;
+        }
+        // cout << r << '\n';
+        ans += totGain;
+        oper -= totOper;
+        // cout << ans << ' ' << oper << ' ' << r << '\n';
+        for (ll i = 0; i < n; i++) {
+            if (value[i] <= r) continue;
+            ll diff = value[i] - r;
+            ll time = (diff + decay[i] - 1)/decay[i];
+            value[i] = value[i] - time * decay[i];
+        }
+        priority_queue<pair<ll,ll>> q;
+        for (ll i = 0; i < n; i++) q.push({value[i], i});
+        while (q.size() && oper) {
+            auto p = q.top();
+            q.pop();
+            if (p.first <= 0) break;
+            oper--;
+            ans += p.first;
+            ans %= MOD;
+            q.push({p.first - decay[p.second], p.second});
+        }
+        return ans;
+    }
+};
