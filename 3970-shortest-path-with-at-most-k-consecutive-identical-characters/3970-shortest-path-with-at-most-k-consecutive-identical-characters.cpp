e82dc300dff1b36e88888d3ad8489d4135f73fd5
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-22 19:55:17 +0700

    Time: 382 ms (16.67%), Space: 506.6 MB (16.67%) - LeetHub

---
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -0,0 +1,43 @@
+int dp[50005][55][27];
+class Solution {
+public:
+    int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
+        vector<vector<pair<int,int>>> g(n);
+        for (auto &e : edges) {
+            g[e[0]].push_back({e[1],e[2]});
+        }
+        
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= k; j++)
+                for (int c = 0; c < 26; c++) dp[i][j][c] = INT_MAX;
+
+        dp[0][1][labels[0] - 'a'] = 0;
+        set<array<int,4>> S;
+        S.insert({0, 0, 1, labels[0] - 'a'});
+        while (S.size()) {
+            auto a = *S.begin();
+            S.erase(S.begin());
+            int w_u = a[0];
+            int u = a[1];
+            int repeat = a[2];
+            int c = a[3];
+
+            if (w_u > dp[u][repeat][c]) continue;
+
+            for (auto [v, w_uv] : g[u]) {
+                int nrepeat = (c == (labels[v] - 'a')) ? repeat + 1 : 1;
+                int nc = labels[v] - 'a';
+                if (nrepeat > k) continue;
+                if (dp[v][nrepeat][nc] > w_u + w_uv) {
+                    S.erase({dp[v][nrepeat][nc], v, nrepeat, nc});
+                    dp[v][nrepeat][nc] = w_u + w_uv;
+                    S.insert({dp[v][nrepeat][nc], v, nrepeat, nc});
+                }
+            }
+        }
+        int ans = INT_MAX;
+        for (int i = 1; i <= k; i++)
+            for (int c = 0; c < 26; c++) ans = min(ans, dp[n - 1][i][c]);
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
