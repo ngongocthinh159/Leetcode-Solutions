0b82085093fbbdc860d6959fbc9ebacbe572e115
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-22 19:39:52 +0700

    Time: 204 ms (28.57%), Space: 350.8 MB (42.86%) - LeetHub

---
+++ 3965-finish-time-of-tasks-i/3965-finish-time-of-tasks-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> g;
+    long long res;
+    long long  dfs(int u, int p, vector<int> &baseTime) {
+        if (g[u].size() == 1 && g[u][0] == p) {
+            return baseTime[u];
+        }
+        long long e = LLONG_MAX;
+        long long l = LLONG_MIN;
+        for (auto v : g[u]) if (v != p) {
+            auto p = dfs(v, u, baseTime);
+            e = min(e, p);
+            l = max(l, p);
+        }
+        long long fin = (e != LLONG_MAX ? (2 * l - e) : 0) + baseTime[u];
+        if (u == 0) res = fin;
+        return fin;
+    }
+    long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
+        g.resize(n);
+        for (auto &e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        dfs(0, -1, baseTime);
+        return res;
+    }
+};
