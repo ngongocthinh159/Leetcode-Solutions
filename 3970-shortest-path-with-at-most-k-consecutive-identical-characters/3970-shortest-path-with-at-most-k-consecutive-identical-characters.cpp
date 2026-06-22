a8f620ee450f4336df531e08cc68dfc64720a9cf
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-22 19:57:31 +0700

    Time: 174 ms (50%), Space: 227.3 MB (66.67%) - LeetHub

--- 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -1,4 +1,4 @@
-int dp[50005][55][27];
+int dp[50005][55];
 class Solution {
 public:
     int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
@@ -6,38 +6,35 @@ public:
         for (auto &e : edges) {
             g[e[0]].push_back({e[1],e[2]});
         }
-        
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= k; j++)
-                for (int c = 0; c < 26; c++) dp[i][j][c] = INT_MAX;
 
-        dp[0][1][labels[0] - 'a'] = 0;
-        set<array<int,4>> S;
-        S.insert({0, 0, 1, labels[0] - 'a'});
+        for (int i = 0; i <= n; i++)
+            for (int j = 0; j <= k; j++) dp[i][j] = INT_MAX;
+
+        dp[0][1] = 0;
+        set<array<int,3>> S;
+        S.insert({0, 0, 1});
         while (S.size()) {
             auto a = *S.begin();
             S.erase(S.begin());
             int w_u = a[0];
             int u = a[1];
             int repeat = a[2];
-            int c = a[3];
 
-            if (w_u > dp[u][repeat][c]) continue;
+            if (w_u > dp[u][repeat]) continue;
 
             for (auto [v, w_uv] : g[u]) {
-                int nrepeat = (c == (labels[v] - 'a')) ? repeat + 1 : 1;
+                int nrepeat = (labels[v] == labels[u]) ? repeat + 1 : 1;
                 int nc = labels[v] - 'a';
                 if (nrepeat > k) continue;
-                if (dp[v][nrepeat][nc] > w_u + w_uv) {
-                    S.erase({dp[v][nrepeat][nc], v, nrepeat, nc});
-                    dp[v][nrepeat][nc] = w_u + w_uv;
-                    S.insert({dp[v][nrepeat][nc], v, nrepeat, nc});
+                if (dp[v][nrepeat] > w_u + w_uv) {
+                    S.erase({dp[v][nrepeat], v, nrepeat});
+                    dp[v][nrepeat] = w_u + w_uv;
+                    S.insert({dp[v][nrepeat], v, nrepeat});
                 }
             }
         }
         int ans = INT_MAX;
-        for (int i = 1; i <= k; i++)
-            for (int c = 0; c < 26; c++) ans = min(ans, dp[n - 1][i][c]);
+        for (int i = 1; i <= k; i++) ans = min(ans, dp[n - 1][i]);
         return ans == INT_MAX ? -1 : ans;
     }
 };
