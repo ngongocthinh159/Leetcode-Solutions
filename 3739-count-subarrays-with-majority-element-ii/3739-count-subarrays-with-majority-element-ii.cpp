57691fd20d92ed24142d282cf3e46682dadb164c
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-06-27 09:56:58 +0700

    Time: 141 ms (38.82%), Space: 155.2 MB (28.23%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,68 @@
+struct FenwickTree {
+    vector<long long> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector<long long> const &a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    long long sum(int r) {
+        long long ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
+    }
+
+    long long sum(int l, int r) {
+        if (l > r) return 0;
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, long long delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+};
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        // f[target_i] - f[target_j] > (i - j) / 2
+        // 2 * f[i] - 2 * f[j] > (i - j)
+        // 2 * f[i] - i > 2 * f[j] - j 
+        // -> 2 * f[j] - j <= 2 * f[i] - i - 1
+        int n = nums.size();
+
+        vector<long long> v;
+        v.push_back(1);
+        long long f = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) f++;
+            v.push_back(f*2 - i);
+            v.push_back(f*2 - i - 1);
+        }
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+        unordered_map<long long,int> toIdx;
+        for (int i = 0; i < int(v.size()); i++) toIdx[v[i]] = i;
+        
+        int sz = v.size();
+        FenwickTree tree(sz);
+        tree.add(toIdx[1], 1);
+        f = 0;
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) f++;
+
+            ans += tree.sum(0, toIdx[f*2 - i - 1]);
+
+            tree.add(toIdx[f*2 - i], 1);
+        }
+        return ans;
+    }
+};
