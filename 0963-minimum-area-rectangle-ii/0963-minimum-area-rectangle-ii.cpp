e2e7e95fd99c67bd1ae24944b5233a43b1836561
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-07-09 20:51:28 +0700

    Time: 55 ms (34.24%), Space: 38 MB (10.33%) - LeetHub

--- 0963-minimum-area-rectangle-ii/0963-minimum-area-rectangle-ii.cpp
+++ 0963-minimum-area-rectangle-ii/0963-minimum-area-rectangle-ii.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    bool isPerpendicular(vector<int> &a, vector<int> &b) {
-        return (a[0] * b[0] + a[1] * b[1]) == 0;
+    long long getCenter(vector<int> &a, vector<int> &b) {
+        int x = a[0] + b[0];
+        int y = a[1] + b[1];
+        return x + 1ll * y * 1e8;
+    }
+    long long getLen(vector<int> &a, vector<int> &b) {
+        return 1ll * (a[0] - b[0]) * (a[0] - b[0]) + 1ll * (a[1] - b[1]) * (a[1] - b[1]);
+    }
+    long long getLenVec(vector<int> &c) {
+        return c[0] * c[0] + c[1] * c[1];
     }
     double minAreaFreeRect(vector<vector<int>>& points) {
         int n = points.size();
         double ans = 1e18;
-        unordered_map<int,unordered_map<int,bool>> vis;
-        for (auto &p : points) vis[p[0]][p[1]] = 1;
+        unordered_map<long long,unordered_map<long long,vector<array<int,2>>>> M;
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                for (int k = 0; k < n; k++) if (i != j && i != k && j != k) {
-                    vector<int> ki = {points[i][0] - points[k][0], points[i][1] - points[k][1]};
-                    vector<int> kj = {points[j][0] - points[k][0], points[j][1] - points[k][1]};
-                    if (isPerpendicular(ki, kj)) {
-                        vector<int> c = {points[i][0] + points[j][0], points[i][1] + points[j][1]};
-                        vector<int> delta = {2*points[k][0] - c[0], 2*points[k][1] - c[1]};
-                        vector<int> r = {c[0] - delta[0], c[1] - delta[1]};
-                        if (r[0] % 2 != 0 || r[1] % 2 != 0) continue;
+            for (int j = i + 1; j < n; j++) {
+                M[getCenter(points[i], points[j])][getLen(points[i], points[j])].push_back({i, j});
+            }
 
-                        r[0] /= 2;
-                        r[1] /= 2;
-                        
-                        if (vis[r[0]][r[1]]) {
-                            ans = min(ans, sqrt ( 1ll * (ki[0] * ki[0] + ki[1] * ki[1]) * (kj[0] * kj[0] + kj[1] * kj[1]) ));
-                        }
+        for (auto &[center, f] : M)
+            for (auto &[len, list] : f) {
+                int m = list.size();
+                for (int i = 0; i < m; i++)
+                    for (int j = i + 1; j < m; j++) {
+                        auto [i1, j1] = list[i];
+                        auto [i2, j2] = list[j];
+                        vector<int> a = {points[i1][0] - points[i2][0], points[i1][1] - points[i2][1]};
+                        vector<int> b = {points[i1][0] - points[j2][0], points[i1][1] - points[j2][1]};
+                        double area = sqrt(getLenVec(a) * getLenVec(b));
+                        ans = min(ans, area);
                     }
-                }
+            }
+        
         return ans == 1e18 ? 0 : ans;
     }
 };
