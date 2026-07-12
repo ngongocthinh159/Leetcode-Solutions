12ade6751c6e2c08cd994cb3ab9331c5efcb156f
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2026-07-12 18:31:33 +0700

    Time: 136 ms (60%), Space: 26.2 MB (50.53%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
@@ -5,27 +5,10 @@ public:
         vector<array<int,2>> moves = {{1,0},{0,1},{-1,0},{0,-1}};
         for (auto &c : circles) {
             int d = c[2];
-            unordered_set<long long> vis;
-            deque<array<int,2>> q;
-            q.push_front({c[0], c[1]});
-            vis.insert(c[0] * 1e5 + c[1]);
-            while (q.size()) {
-                for (int sz = q.size(); sz; sz--) {
-                    auto [i, j] = q.back();
-                    S.insert(i * 1e5 + j);
-                    q.pop_back();
-                    for (auto &move : moves) {
-                        int I = i + move[0];
-                        int J = j + move[1];
-                        int D = (I - c[0]) * (I - c[0]) + (J - c[1]) * (J - c[1]);
-                        long long x = I * 1e5 + J;
-                        if (D <= d * d && !vis.count(x)) {
-                            vis.insert(x);
-                            q.push_front({I, J});
-                        }
-                    }
-                }
-            }
+            int x = c[0];
+            int y = c[1];
+            for (int i = x - d; i <= x + d; i++)
+                for (int j = y - d; j <= y + d; j++) if ((i - x) * (i - x) + (j - y) * (j - y) <= d * d) S.insert(i * 1e5 + j);
         }
         return S.size();
     }
